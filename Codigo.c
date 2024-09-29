687d9c1852b858e482af961bad8688280b4b96cb
dany14101 <danylinan74@gmail.com>
2024-09-29 23:53:32 +0000

    cambio1

---
+++ Codigo.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+typedef struct LISTA
+{ 
+	char *val;
+	struct LISTA *sig;
+}LISTA;
+
+LISTA* crean(FILE *arch);
+LISTA* crear(FILE *arch);
+void captura(FILE *arch, LISTA **p2);
+void imprime(LISTA  *p2);
+int main()
+{ 
+	LISTA *p2;
+	FILE *arch;
+	arch=fopen("Rules.txt","r");
+	p2=crear(arch);
+	captura(arch,&p2);
+	imprime(p2)
+;	fclose(arch);
+}    
+
+//Crea el nodo
+LISTA * crean(FILE *arch)
+{
+	LISTA *p;
+	p=(LISTA*)malloc(sizeof(LISTA));
+	if(p)
+	{
+		p->val=(char*)malloc(sizeof(char)*300);
+			if(p->val)
+			{
+				p->val[0]='\0';
+			}
+		p->sig=NULL;
+	}
+	return(p);
+}
+//Funcion de crea lista
+LISTA* crear(FILE *arch)
+{
+	LISTA *p1;
+	p1=crean(arch);
+	return(p1);
+}
+
+//Funcion captura por linea en un nodo differente
+void captura(FILE *arch, LISTA **p2)
+{
+	LISTA *aux=*p2,*nue;
+	char cadena[300];
+	while(fgets(cadena,sizeof(cadena),arch))
+	{
+		cadena[strcspn(cadena,"\n")]=0;
+		strncpy(aux->val,cadena,299);
+		nue=crean(arch);
+		aux->sig=nue;
+		aux=nue;
+	}
+}
+
+//Funcion de imprimir
+void imprime(LISTA  *p2)
+{
+	LISTA *aux=p2;
+	while(aux!= NULL)
+	{
+		printf("%s\n",aux->val);
+		aux=aux->sig;
+	}
+}
+
